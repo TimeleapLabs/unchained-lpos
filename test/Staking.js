f5b09ec4a5a6fce48db9d5451a0321152bfbb0fa
Pouya <pouya@kenshi.io>
2024-03-08 16:17:06 +0100

    Fix tests

--- test/Staking.js
+++ test/Staking.js
@@ -1,5 +1,3 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
 const { time } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("Unchained Staking", function () {});
